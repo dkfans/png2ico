c6ff35cdb53636c6d5e0e5fa126142aae49b723d
xtremeqg <xtremeq_g@hotmail.com>
2024-10-26 22:43:41 +0300

    Removed trailing whitespace

--- png2ico.cpp
+++ png2ico.cpp
@@ -38,7 +38,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 /*
 Notes about transparent and inverted pixels:
  Handling of transparent pixels is inconsistent in Windows. Sometimes a
- pixel with an AND mask value of 1 is just transparent (i.e. its color 
+ pixel with an AND mask value of 1 is just transparent (i.e. its color
  value is ignored), sometimes the color value is XORed with the background to
  give some kind of inverted effect. A closer look at bmp.txt suggests that
  the latter behaviour is the correct one but because it often doesn't happen
@@ -142,26 +142,26 @@ bool checkTransparent3(png_bytep, png_data&)
   return false;
 };
 
-//returns true if color reduction resulted in at least one of the image's colors 
+//returns true if color reduction resulted in at least one of the image's colors
 //being mapped to a palette color with a quadratic distance of more than
 //color_reduce_warning_threshold
 bool convertToIndexed(png_data& img, bool hasAlpha)
 {
   int maxColors=img.requested_colors;
-  
+
   size_t palSize=sizeof(png_color)*256; //must reserve space for 256 entries here because write loop in main() expects it
   img.palette=(png_colorp)malloc(palSize);
   memset(img.palette,0,palSize); //must initialize whole palette because write loop in main() expects it
   img.num_palette=0;
-  
+
   checkTransparent_t checkTrans=checkTransparent1;
   int bytesPerPixel=4;
   if (!hasAlpha)
   {
     bytesPerPixel=3;
-    checkTrans=checkTransparent3;  
+    checkTrans=checkTransparent3;
   };
-  
+
   //first pass: gather all colors, make sure
   //alpha channel (if present) contains only 0 and 255
   //if an alpha channel is present, set all transparent pixels to RGBA (0,0,0,0)
@@ -169,7 +169,7 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
   //pixels will not get a mapping yet (-1)
   hash_map<unsigned int,signed int> mapQuadToPalEntry;
   png_bytep* row_pointers=png_get_rows(img.png_ptr, img.info_ptr);
-  
+
   for (int y=img.height-1; y>=0; --y)
   {
     png_bytep pixel=row_pointers[y];
@@ -177,8 +177,8 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
     {
       unsigned int quad=pixel[0]+(pixel[1]<<8)+(pixel[2]<<16);
       bool trans=(*checkTrans)(pixel,img);
-     
-      if (hasAlpha) 
+
+      if (hasAlpha)
       {
         if (trans)
         {
@@ -189,20 +189,20 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
           quad=0;
         }
         else pixel[3]=255;
-        
+
         quad+=(pixel[3]<<24);
       }
       else if (!trans) quad+=(255<<24);
-      
-      if (trans) 
+
+      if (trans)
         mapQuadToPalEntry[quad]=0;
-      else  
+      else
         mapQuadToPalEntry[quad]=-1;
-    
+
       pixel+=bytesPerPixel;
-    };  
+    };
   };
-  
+
   //always allocate entry 0 to black and entry 1 to white because
   //sometimes AND mask is interpreted as color index
   img.num_palette=2;
@@ -212,15 +212,15 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
   img.palette[1].red=255;
   img.palette[1].green=255;
   img.palette[1].blue=255;
-  
+
   mapQuadToPalEntry[255<<24]=0; //map (non-transparent) black to entry 0
   mapQuadToPalEntry[255+(255<<8)+(255<<16)+(255<<24)]=1; //map (non-transparent) white to entry 1
-  
+
   if (mapQuadToPalEntry.size()*img.requested_colors>slow_reduction_warn_threshold)
   {
     fprintf(stdout,"Please be patient. My color reduction algorithm is really slow.\n");
   };
-  
+
   //Now fill up the palette with colors from the image by repeatedly picking the
   //color most different from the previously picked colors and adding this to the
   //palette. This is done to make sure that in case there are more image colors than
@@ -253,8 +253,8 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
           dist+=temp*temp;
           if (dist<minDist) minDist=dist;
           distSum+=dist;
-        };  
-        
+        };
+
         if (minDist>mdqMinDist || (minDist==mdqMinDist && distSum>mdqDistSum))
         {
           mostDifferentQuad=quad;
@@ -263,7 +263,7 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
         };
       };
     };
-    
+
     if (mdqMinDist>0) //if we have found a most different quad, add it to the palette
     {                  //and map it to the new palette entry
       int palentry=img.num_palette;
@@ -300,11 +300,11 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
         dist+=temp*temp;
         if (dist<minDist) { minDist=dist; bestIndex=i; };
       };
-      
-      mapping.second=bestIndex; 
+
+      mapping.second=bestIndex;
     };
   };
-  
+
   //Adjust all palette entries (except for 0 and 1) to be the mean of all
   //colors mapped to it
   for (int i=2; i<img.num_palette; ++i)
@@ -327,7 +327,7 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
         ++numMappings;
       };
     };
-    
+
     if (numMappings>0)
     {
       img.palette[i].red=(red+red+numMappings)/(numMappings+numMappings);
@@ -336,7 +336,7 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
     };
   };
 
-  //Now determine if a non-transparent source color got mapped to a target color that 
+  //Now determine if a non-transparent source color got mapped to a target color that
   //has a distance that exceeds the threshold
   bool tooManyColors=false;
   stop=mapQuadToPalEntry.end();
@@ -360,12 +360,12 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
       if (dist>color_reduce_warning_threshold) tooManyColors=true;
     };
   };
-  
+
 
   int transLineLen=andMaskLineLen(img);
   int transLinePad=transLineLen - ((img.width+7)/8);
   img.transMap=(png_bytepp)malloc(img.height*sizeof(png_bytep));
-  
+
   //second pass: convert RGB to palette entries
   for (int y=img.height-1; y>=0; --y)
   {
@@ -374,14 +374,14 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
     int count8=0;
     int transbyte=0;
     png_bytep transPtr=img.transMap[y]=(png_bytep)malloc(transLineLen);
-    
+
     for (unsigned i=0; i<img.width; ++i)
     {
       bool trans=((*checkTrans)(pixel,img));
       unsigned int quad=pixel[0]+(pixel[1]<<8)+(pixel[2]<<16);
       if (!trans) quad+=(255<<24); //NOTE: alpha channel has already been set to 255 for non-transparent pixels, so this is correct even for images with alpha channel
-      
-      if (trans) ++transbyte; 
+
+      if (trans) ++transbyte;
       if (++count8==8)
       {
         *transPtr++ = transbyte;
@@ -389,7 +389,7 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
         transbyte=0;
       };
       transbyte+=transbyte; //shift left 1
-      
+
       int palentry=mapQuadToPalEntry[quad];
       row[i]=palentry;
       pixel+=bytesPerPixel;
@@ -397,7 +397,7 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
 
     for(int i=0; i<transLinePad; ++i) *transPtr++ = 0;
   };
-  
+
   return tooManyColors;
 };
 
@@ -415,7 +415,7 @@ int pack(png_bytep row,int width,int nbits)
   for (int i=0; i<width; ++i)
   {
     outByte+=(row[i]&ander);
-    if (++count==pixelsPerByte) 
+    if (++count==pixelsPerByte)
     {
       row[outIndex]=outByte;
       count=0;
@@ -425,13 +425,13 @@ int pack(png_bytep row,int width,int nbits)
     outByte<<=nbits;
   };
 
-  if (count>0) 
+  if (count>0)
   {
     outByte<<=nbits*(pixelsPerByte-count);
     row[outIndex]=outByte;
     ++outIndex;
-  };  
-  
+  };
+
   return outIndex;
 };
 
@@ -446,18 +446,18 @@ void usage()
 int main(int argc, char* argv[])
 {
   if (argc<3) usage();
-  
-  if (argc-2 > word_max) 
+
+  if (argc-2 > word_max)
   {
     fprintf(stderr,"Too many PNG files\n");
     exit(1);
   };
-  
+
   vector<png_data> pngdata;
-  
+
   static int numColors=256; //static to get rid of longjmp() clobber warning
   static const char* outfileName=NULL;
-  
+
   //i is static because used in a setjmp() block
   for (static int i=1; i<argc; ++i)
   {
@@ -479,9 +479,9 @@ int main(int argc, char* argv[])
       numColors=num;
       continue;
     };
-    
+
     if (outfileName==NULL) { outfileName=argv[i]; continue; };
-    
+
     FILE* pngfile=fopen(argv[i],"rb");
     if (pngfile==NULL)  {perror(argv[i]); exit(1);};
     png_byte header[8];
@@ -491,18 +491,18 @@ int main(int argc, char* argv[])
       fprintf(stderr,"%s: Not a PNG file\n",argv[i]);
       exit(1);
     };
-    
+
     png_data data;
     data.requested_colors=numColors;
     for (data.col_bits=1; (1<<data.col_bits)<numColors; ++data.col_bits);
-    
+
     data.png_ptr=png_create_read_struct
                    (PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     if (!data.png_ptr)
     {
       fprintf(stderr,"png_create_read_struct error\n");
       exit(1);
-    };  
+    };
 
     data.info_ptr=png_create_info_struct(data.png_ptr);
     if (!data.info_ptr)
@@ -519,25 +519,25 @@ int main(int argc, char* argv[])
       fprintf(stderr,"png_create_info_struct error\n");
       exit(1);
     };
-    
+
     if (setjmp(png_jmpbuf(data.png_ptr)))
     {
       png_destroy_read_struct(&data.png_ptr, &data.info_ptr, &data.end_info);
       fprintf(stderr,"%s: PNG error\n",argv[i]);
       exit(1);
     };
-    
+
     png_init_io(data.png_ptr, pngfile);
     png_set_sig_bytes(data.png_ptr,8);
     int trafo=PNG_TRANSFORM_PACKING|PNG_TRANSFORM_STRIP_16|PNG_TRANSFORM_EXPAND;
     png_read_png(data.png_ptr, data.info_ptr, trafo , NULL);
 
     int bit_depth, color_type, interlace_type, compression_type, filter_method;
-    png_get_IHDR(data.png_ptr, data.info_ptr, &data.width, &data.height, &bit_depth, &color_type, 
+    png_get_IHDR(data.png_ptr, data.info_ptr, &data.width, &data.height, &bit_depth, &color_type,
                  &interlace_type, &compression_type, &filter_method);
-                 
-    
-    
+
+
+
     if ( (data.width&7)!=0 || data.width>=256 || data.height>=256)
     {
       //I don't know if the following is really a requirement (bmp.txt says that
@@ -547,13 +547,13 @@ int main(int argc, char* argv[])
       fprintf(stderr,"%s: Width must be multiple of 8 and <256. Height must be <256.\n",argv[i]);
       exit(1);
     };
-    
+
     if ((color_type & PNG_COLOR_MASK_COLOR)==0)
     {
       fprintf(stderr,"%s: Grayscale image not supported\n",argv[i]);
       exit(1);
     };
-    
+
     if (color_type==PNG_COLOR_TYPE_PALETTE)
     {
       fprintf(stderr,"This can't happen. PNG_TRANSFORM_EXPAND transforms image to RGB.\n");
@@ -565,25 +565,25 @@ int main(int argc, char* argv[])
       {
         fprintf(stderr,"%s: Warning! Color reduction may not be optimal!\nIf the result is not satisfactory, reduce the number of colors\nbefore using png2ico.\n",argv[i]);
       };
-    };  
-    
+    };
+
     pngdata.push_back(data);
-    
+
     fclose(pngfile);
   };
-  
+
 
   if (outfileName==NULL || pngdata.size()<1) usage();
-  
+
   FILE* outfile=fopen(outfileName,"wb");
   if (outfile==NULL) {perror(argv[1]); exit(1);};
-  
+
   writeWord(outfile,0); //idReserved
   writeWord(outfile,1); //idType
   writeWord(outfile,pngdata.size()); //idCount
-  
+
   int offset=6+pngdata.size()*16;
-  
+
   vector<png_data>::const_iterator img;
   for(img=pngdata.begin(); img!=pngdata.end(); ++img)
   {
@@ -598,8 +598,8 @@ int main(int argc, char* argv[])
     writeDWord(outfile,offset); //dwImageOffset
     offset+=resSize;
   };
-  
-  
+
+
   for(img=pngdata.begin(); img!=pngdata.end(); ++img)
   {
     writeDWord(outfile,40); //biSize
@@ -622,7 +622,7 @@ int main(int argc, char* argv[])
       col[3]=0;
       if (fwrite(col,4,1,outfile)!=1) {perror("Write error"); exit(1);};
     };
-    
+
     png_bytep* row_pointers=png_get_rows(img->png_ptr, img->info_ptr);
     for (int y=img->height-1; y>=0; --y)
     {
@@ -631,15 +631,13 @@ int main(int argc, char* argv[])
       if (fwrite(row,newLength,1,outfile)!=1) {perror("Write error"); exit(1);};
       for(int i=0; i<xorMaskLineLen(*img)-newLength; ++i) writeByte(outfile,0);
     };
-    
+
     for (int y=img->height-1; y>=0; --y)
     {
       png_bytep transPtr=img->transMap[y];
       if (fwrite(transPtr,andMaskLineLen(*img),1,outfile)!=1) {perror("Write error"); exit(1);};
     };
   };
-  
+
   fclose(outfile);
 };
-
-
